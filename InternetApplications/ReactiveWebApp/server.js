f8e38cca3f5b200f51062decb8609b95d07eb69b
seangaff <gaffnese@gmail.com>
2022-10-23 22:43:09 +0100

    Complete UI

--- InternetApplications/ReactiveWebApp/server.js
+++ InternetApplications/ReactiveWebApp/server.js
@@ -4,7 +4,6 @@
 //api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=e3118b4d41776ba1cbdeefefd50ae3b0
 
 const express = require("express");
-const { validateHeaderName } = require("http");
 const https = require("https");
 const app = express();
 const port = 3000;
@@ -18,10 +17,14 @@ function sendForecast(req, res) {
 	let city = req.params.city;
 	let request = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=e3118b4d41776ba1cbdeefefd50ae3b0&units=metric`;
 	getJson(request).then(async (response) => {
-		let lat = response.city.coord.lat;
-		let lon = response.city.coord.lon;
-		let aqi = await getAQI(lat, lon);
-		res.send(genReponse(response, aqi));
+		if (response.cod == 200) {
+			let lat = response.city.coord.lat;
+			let lon = response.city.coord.lon;
+			let aqi = await getAQI(lat, lon);
+			res.send(genReponse(response, aqi));
+		} else {
+			res.send("Error");
+		}
 	});
 }
 
@@ -72,6 +75,16 @@ function genReponse(weatherData, aqiData) {
 	let avgAQI = 0;
 	let aqiStatus = "do not";
 	let summary = [];
+	let location = weatherData.city.name + ", " + weatherData.city.country;
+	const weekday = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
 
 	//check for rain
 	for (let i = 0; i < weatherData.list.length; i++) {
@@ -108,25 +121,41 @@ function genReponse(weatherData, aqiData) {
 	}
 
 	// generate summary
-	// temperature, Wind Speed and Rainfall level. 8 per day, 5 days
-	for (let i = 0; i < weatherData.list.length; i += 8) {
-		let dailyTemp = 0;
-		let dailyWind = 0;
-		let dailyRain = 0;
+	// temperature, Wind Speed and Rainfall level. 8 per day, 4 days
+	let d = 0;
+	let first = new Date(weatherData.list[d].dt_txt).getDay();
+	let today = new Date().getDay();
+	while (first == today) {
+		d++;
+		first = new Date(weatherData.list[d].dt_txt).getDay();
+	}
+
+	for (let i = d; i < d + 4 * 8; i += 8) {
+		let dailyTemp = 0; //average temp
+		let dailyWind = 0; //average wind
+		let dailyRain = 0; //total rain
+		let icon = "";
+
+		let day = new Date(weatherData.list[i].dt_txt).getDay();
 		for (let j = i; j < i + 8; j++) {
 			dailyTemp += weatherData.list[j].main.temp;
 			dailyWind += weatherData.list[j].wind.speed;
 			if (weatherData.list[j].rain) {
 				dailyRain += weatherData.list[j].rain["3h"];
 			}
+			if (j == i + 4) {
+				icon =
+					"http://openweathermap.org/img/w/" +
+					weatherData.list[j].weather[0].icon +
+					".png";
+			}
 		}
-		// dailyTemp = ;
-		// dailyRain = ;
-		// dailyWind = ;
 		summary.push({
 			dailyTemp: Math.round(dailyTemp / 8),
 			dailyWind: Math.round(dailyWind / 8),
-			dailyRain: Math.round(dailyRain / 8),
+			dailyRain: Math.round(dailyRain),
+			weekday: weekday[day],
+			icon: icon,
 		});
 	}
 	//set response
@@ -139,6 +168,7 @@ function genReponse(weatherData, aqiData) {
 		Summary: summary,
 		aqiStatus: aqiStatus,
 		avgAQI: avgAQI,
+		location: location,
 	};
 	return response;
 }
